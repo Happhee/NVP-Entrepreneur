f72340658f7996528ed6c2b40422175e6f18bfcc
seohee0112 <ghdtj7@naver.com>
2022-01-06 14:28:10 +0900

    [CONNECT] 아이디 비밀번호 설정 component, container, screen연결

---
+++ src/containers/auth/IdPassword.js
@@ -0,0 +1,14 @@
+import { connect } from "react-redux";
+import IdPassword from "../../components/auth/IdPassword";
+
+function mapReduxStateToReactProps(state) {
+    return state;
+}
+
+function mapReduxDispatchToReactProps(dispatch) {
+    return {
+
+    }
+}
+
+export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(IdPassword);
---
+++ src/screens/AuthStack/IdPassword.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import IdPassword from '../../containers/auth/IdPassword';
+function IdPasswordScreen() {
+    return (
+        <View>
+            <IdPassword />
+        </View>
+    )
+}
+export default IdPasswordScreen;
