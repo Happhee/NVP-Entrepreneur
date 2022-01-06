b9a018415d51dbf17936f0d58076eb294c90a80b
seohee0112 <ghdtj7@naver.com>
2022-01-06 13:38:52 +0900

    [CREATE] 사업가 리듀서 생성

---
+++ src/redux/reducers/entrepreneur.js
@@ -0,0 +1,21 @@
+import AsyncStorage from "@react-native-community/async-storage";
+
+const initialState = {
+    id: '',
+    name: '',
+    password: '',
+    store_num: '',
+    store_name: '',
+    store_phone: '',
+    store_kind: '',
+    store_location: '',
+    store_address: '',
+    filename: '',
+    loading: false
+}
+
+function entrepreneurReducer(state = initialState, action) {
+
+}
+
+export default entrepreneurReducer;
