bacbc567588a12aff803e6b1d2484d74a0f06b7a
seohee0112 <ghdtj7@naver.com>
2022-01-06 14:28:04 +0900

    [CONNECT] 로그인 component, container, screen연결

---
+++ src/containers/auth/Login.js
@@ -0,0 +1,13 @@
+import { connect } from "react-redux";
+import Login from "../../components/auth/Login";
+function mapReduxStateToReactProps(state) {
+    return state;
+}
+
+function mapReduxDispatchToReactProps(dispatch) {
+    return {
+
+    }
+}
+
+export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(Login);
---
+++ src/screens/AuthStack/Login.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import Login from '../../containers/auth/Login';
+function LoginScreen() {
+    return (
+        <View>
+            <Login />
+        </View>
+    )
+}
+export default LoginScreen;
