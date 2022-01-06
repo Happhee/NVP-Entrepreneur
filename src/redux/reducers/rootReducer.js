de80cebdee5cee86e6e05db1977201245e44da11
seohee0112 <ghdtj7@naver.com>
2022-01-06 13:39:00 +0900

    [CONNECT] 연결

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from "redux";
-
+import authReducer from "./auth";
+import entrepreneurReducer from "./entrepreneur";
 export const rootReducer = combineReducers({
-
+    auth: authReducer,
+    entrepreneur: entrepreneurReducer,
 });
