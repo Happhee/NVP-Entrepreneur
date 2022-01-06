a006529951f7f468d84ddf704c4021b8e5f910cd
seohee0112 <ghdtj7@naver.com>
2022-01-06 14:28:43 +0900

    [CONNECT] 로그인및 회원가입 스크린 네비게이션에 연결

---
+++ src/navigations/AuthStack.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from '../screens/AuthStack/Login';
+import ChoiceIdPasswordScreen from '../screens/AuthStack/ChoiceIdPassword';
+import EntrepreneurInfoScreen from '../screens/AuthStack/EntrepreneurInfo';
+import FindIdScreen from '../screens/AuthStack/FindId';
+import FindPasswordScreen from '../screens/AuthStack/FindPassword';
+import StoreInfoScreen from '../screens/AuthStack/StoreInfo';
+import IdPasswordScreen from '../screens/AuthStack/IdPassword';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStackScreen = () => {
+    retunr(
+        <Stack.Navigator>
+            <Stack.Screen name='Login' component={LoginScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='ChoiceIdPassword' component={ChoiceIdPasswordScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='EntrepreneurInfo' component={EntrepreneurInfoScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='FindId' component={FindIdScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='FindPassword' component={FindPasswordScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='StoreInfo' component={StoreInfoScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+            <Stack.Screen name='IdPassword' component={IdPasswordScreen}
+                options={{
+                    title: '',
+                    headerBackTitleVisible: false
+                }} />
+
+        </Stack.Navigator>
+    )
+}
