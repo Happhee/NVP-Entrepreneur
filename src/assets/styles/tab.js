0f8e65554995edbd8fe77f92ad4ae490abdee75b
seohee0112 <ghdtj7@naver.com>
2022-01-06 16:51:47 +0900

    [ADD] 색관련 스타일 및 로고 추가

---
+++ src/assets/styles/tab.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+import colors from "../colors/color";
+const tabStyles = StyleSheet.create({
+    bottomTab: {
+        backgroundColor: colors.tabColor,
+        padding: 20,
+        fontSize: 20,
+    }, backgroundColor: colors.tabColor,
+});
+
+export default tabStyles;
