5eb349971f34a9a968d4d613c99f572cb9c53101
seohee0112 <ghdtj7@naver.com>
2022-01-20 11:03:20 +0900

    Docs : Nfc 관련 문서 구조화
    Nfc 자바스크립트 파일 component, container, screen으로 분할하여 구현

--- src/containers/home/MyNfc.js
+++ src/containers/home/MyNfc.js
@@ -1,5 +1,7 @@
 import { connect } from "react-redux";
 import MyNfc from "../../components/home/MyNfc";
+import WriteNfc from "../../components/home/MyNfc";
+import { registerNfcId } from "../../redux/actions/entrepreneur";
 
 function mapReduxStateToReactProps(state) {
     return state;
@@ -7,8 +9,12 @@ function mapReduxStateToReactProps(state) {
 
 function mapReduxDispatchToReactProps(dispatch) {
     return {
+        registerNfcId: (dataToSubmit) => {
+            console.log(dataToSubmit)
+            dispatch(registerNfcId(dataToSubmit))
+        }
 
     }
 }
 
-export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(MyNfc);
+export default connect(mapReduxStateToReactProps, mapReduxDispatchToReactProps)(WriteNfc);
