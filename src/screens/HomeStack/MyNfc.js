97ac2a2bc8bd218cdcf7e6cfd68bdfa4a327a648
jihoon98 <tlswlgns1003@sju.ac.kr>
2022-01-28 00:34:35 +0900

    .

--- src/screens/HomeStack/MyNfc.js
+++ src/screens/HomeStack/MyNfc.js
@@ -1,147 +1,154 @@
-import React, {Component} from 'react'
-import { View, Text, TouchableOpacity, ScrollView, TextInput,} from 'react-native';
-import NfcManager, {NdefParser, NfcTech} from 'react-native-nfc-manager';
+import React from 'react';
+import { View, Text, Modal, TouchableOpacity, Platform, SafeAreaView, StyleSheet, TextInput, Alert
+} from 'react-native';
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
 
-function strToBytes(str) {
-    let result = [];
-    for (let i = 0; i < str.length; i++) {
-        result.push(str.charCodeAt(i));
-    }
-    return result;
-}
-
-function buildTextPayload(valueToWrite) {
-    const textBytes = strToBytes(valueToWrite);
-    const headerBytes = [0xD1, 0x01, (textBytes.length + 3), 0x54, 0x02, 0x65, 0x6e];//영어 바이트
-    return [...headerBytes, ...textBytes];
-}
-
-class WriteNfc extends Component {
-    constructor(props) {
+class App extends React.Component {
+    constructor(props){
         super(props);
         this.state = {
-            supported: false,
-            enabled: false,
-            isTestRunning: false,
-            text: 'dddddddddddd',
-            parsedText: null,
-            tag: null,
+            modalVisible:false,
+            log: "",
+            text: ""
+        }
+    }
+    componentDidMount() {
+        NfcManager.start();
+    }
+
+    componentWillUnmount() {
+        this._cleanUp();
+    }
+
+    _cleanUp = () => {
+        NfcManager.cancelTechnologyRequest().catch(() => 0);
+    }
+    writeData = async () => {
+        if (!this.state.text){
+            Alert.alert("Nothing to write");
+            return;
+        }
+        try {
+            let tech = Platform.OS === 'ios' ? NfcTech.MifareIOS : NfcTech.NfcA;
+            let resp = await NfcManager.requestTechnology(tech, {
+                alertMessage: 'Ready to do some custom Mifare cmd!'
+            });
+
+            let text = this.state.text;
+            let fullLength = text.length + 7;
+            let payloadLength = text.length + 3;
+
+            let cmd = Platform.OS === 'ios' ? NfcManager.sendMifareCommandIOS : NfcManager.transceive;
+
+            resp = await cmd([0xA2, 0x04, 0x03, fullLength, 0xD1, 0x01]); 
+            resp = await cmd([0xA2, 0x05, payloadLength, 0x54, 0x02, 0x65]); 
+
+            let currentPage = 6;
+            let currentPayload = [0xA2, currentPage, 0x6E];
+
+            for(let i=0; i<text.length; i++){
+                currentPayload.push(text.charCodeAt(i));
+                if (currentPayload.length == 6){
+                    resp = await cmd(currentPayload);
+                    currentPage += 1;
+                    currentPayload = [0xA2, currentPage];
+                }
+            }
+
+            currentPayload.push(254);
+            while(currentPayload.length < 6){
+                currentPayload.push(0);
+            }
+
+            resp = await cmd(currentPayload);
+
+            this.setState({
+                log: resp.toString()
+            })
+
+            this._cleanUp();
+        } catch (ex) {
+            this.setState({
+                log: ex.toString()
+            })
+            this._cleanUp();
         }
     }
 
-    componentDidMount() {
-        NfcManager.isSupported()
-            .then(supported => {
-                this.setState({ supported });
-                if (supported) {
-                    this._startNfc();
-                }
-            })
+    onChangeText = (text) => {
+        this.setState({
+            text
+        })
     }
 
     render() {
-        let { supported, enabled, tag, text, parsedText, isTestRunning} = this.state;
         return (
-            <ScrollView style={{flex: 1}}>
-                <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
+            <SafeAreaView style={styles.container}>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={this.onChangeText}
+                    autoCompleteType="off"
+                    autoCapitalize="none"
+                    autoCorrect={false}
+                    placeholderTextColor="#888888"
+                    placeholder="상호명을 입력하세요" />
 
-                    {
-                        <View style={{padding: 20, marginTop: 20, backgroundColor: '#f0f0f0'}}>
-                            <View style={{flexDirection: 'row', alignItems: 'center'}}>
-                                <Text>입력할 문자</Text>
-                                <TextInput 
-                                    value={text}
-                                    style={{marginLeft: 10, flex: 1}}
-                                    onChangeText={text => this.setState({text})} 
-                                />
-                            </View>
+                <TouchableOpacity
+                    style={styles.buttonWrite}
+                    onPress={this.writeData}>
+                    <Text style={styles.buttonText}>입력</Text>
+                </TouchableOpacity>
 
-                            {!isTestRunning && (
-                                <TouchableOpacity
-                                    style={{ margin: 10 }}
-                                    onPress={() => this._runTest(text)}
-                                >
-                                    <Text style={{ color: 'blue', textAlign: 'center', fontSize: 20 }}>시작</Text>
-                                </TouchableOpacity>
-                            )}
-                            
-                            {isTestRunning && (
-                                <TouchableOpacity
-                                    style={{ margin: 10 }}
-                                    onPress={() => this._cancelTest()}
-                                >
-                                    <Text style={{ color: 'red', textAlign: 'center', fontSize: 20 }}>정지</Text>
-                                </TouchableOpacity>
-                            )}
-                        </View>
-                    }
-
-                    <View style={{alignItems: 'center', justifyContent: 'center', padding: 20, marginTop: 20}}>
-                        <Text >{`태그 콘텐트:`}</Text>
-                        <Text style={{marginTop: 5, color: 'grey'}}>{`${tag ? JSON.stringify(tag) : '---'}`}</Text>
-                        { parsedText && <Text style={{ marginTop: 5, }}>{`(Parsed Text: ${parsedText})`}</Text>}
-                    </View>
-
-                    <TouchableOpacity style={{ marginTop: 20, alignItems: 'center' }} onPress={this._clearMessages}>
-                        <Text style={{color: 'blue'}}>초기화</Text>
-                    </TouchableOpacity>
+                <View style={styles.log}>
+                    <Text>{this.state.log}</Text>
                 </View>
-            </ScrollView>
+            </SafeAreaView>
         )
-    }
-
-    _runTest = textToWrite => {
-        const cleanUp = () => {
-            this.setState({isTestRunning: false});
-            NfcManager.closeTechnology()
-            NfcManager.unregisterTagEvent();
-        }
-
-        const parseText = (tag) => {
-            if (tag.ndefMessage) {
-                return NdefParser.parseText(tag.ndefMessage[0]);
-            }
-            return null;
-        }
-
-        this.setState({isTestRunning: true});
-        NfcManager.registerTagEvent(tag => console.log(tag))
-            .then(() => NfcManager.requestTechnology(NfcTech.Ndef))
-            .then(() => NfcManager.getTag())
-            .then(tag => {
-                console.log(JSON.stringify(tag));
-            })
-            .then(() => NfcManager.getNdefMessage())
-            .then(tag => {
-                let parsedText = parseText(tag);
-                this.setState({tag, parsedText})
-            })
-            .then(() => NfcManager.writeNdefMessage(buildTextPayload(textToWrite)))
-            .then(cleanUp)
-            .catch(err => {
-                console.warn(err);
-                cleanUp();
-            })
-    }
-
-    _cancelTest = () => {
-        NfcManager.cancelTechnologyRequest()
-            .catch(err => console.warn(err));
-    }
-
-    _startNfc = () => {
-        NfcManager.start()
-            .then(() => NfcManager.isEnabled())
-            .then(enabled => this.setState({enabled}))
-            .catch(err => {
-                console.warn(err);
-                this.setState({enabled: false})
-            })
-    }
-
-    _clearMessages = () => {
-        this.setState({tag: null, parsedText: null});
     }
 }
 
-export default WriteNfc;
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+    },
+    textInput: {
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 10,
+        height: 50,
+        textAlign: 'center',
+        color: 'black'
+    },
+    buttonWrite: {
+        marginLeft: 20,
+        marginRight: 20,
+        marginBottom: 10,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        backgroundColor: '#9D2235'
+    },
+    buttonRead: {
+        marginLeft: 20,
+        marginRight: 20,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+        backgroundColor: '#006C5B'
+    },
+    buttonText: {
+        color: '#ffffff'
+    },
+    log: {
+        marginTop: 30,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
+
+export default App;
